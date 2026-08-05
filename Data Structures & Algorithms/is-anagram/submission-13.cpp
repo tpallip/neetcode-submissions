be4067ff562f7bd3657c60f7257a0b63b16d1d8e
Pranay Tummalapalli <31589727+tpallip@users.noreply.github.com>
2026-08-05 22:21:54 +0530

    Add: is-anagram - submission-13

---
+++ Data Structures & Algorithms/is-anagram/submission-13.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+      if(s.length() != t.length()) return false;
+      unordered_map<char, int> freq;
+      
+      for(char c : s){
+        freq[c]++;
+      }
+      
+      for(char c : t){
+        if(--freq[c] < 0) return false;
+      }
+
+      return true;
+    }
+};
